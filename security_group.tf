0b7b5772c2145a673c564d44c90ea6780890ca08
大碗臊子干拌面 <131854300+Simon-in@users.noreply.github.com>
2025-06-12 10:36:21 +0800

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -3,6 +3,7 @@ resource resource_type resource_name {
    资源参数
 }
 */
+
 resource "aws_security_group" "my_sg" {
   name = "my_sg"
 }
@@ -30,7 +31,7 @@ output "igress_cidr_ipv4" {
   value = aws_vpc_security_group_ingress_rule.my_sg_igress_ipv4.cidr_ipv4
 }
 
-# 出站规则可以删除不建
+
 resource "aws_vpc_security_group_egress_rule" "my_sg_egress_ipv4" {
   security_group_id = aws_security_group.my_sg.id
   cidr_ipv4 = "${aws_eip.my_eip.public_ip}/32"
